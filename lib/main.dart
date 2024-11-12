e77c51e0f212fb29b6d2515569ea10de98ba46ff
nadoran78 <nadoran78@gmail.com>
2024-11-12 18:15:59 +0900

    fix: 삭제한 BigCard 위젯 복구

--- lib/main.dart
+++ lib/main.dart
@@ -126,3 +126,33 @@ class GeneratorPage extends StatelessWidget {
     );
   }
 }
+
+class BigCard extends StatelessWidget {
+  const BigCard({
+    super.key,
+    required this.pair,
+  });
+
+  final WordPair pair;
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+
+    final style = theme.textTheme.displayMedium!.copyWith(
+      color: theme.colorScheme.onPrimary,
+    );
+
+    return Card(
+      color: theme.colorScheme.primary,
+      child: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Text(
+          pair.asLowerCase,
+          style: style,
+          semanticsLabel: "${pair.first} ${pair.second}",
+        ),
+      ),
+    );
+  }
+}
